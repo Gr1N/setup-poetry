04356446800e0330ce944fe886c134a8e95291ed
Nikita Grishko <gr1n@protonmail.com>
2020-01-03 19:01:05 +0300

    [chore] fmt, update deps and meta

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from "@actions/core"
+import { wait } from "./wait"
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const ms: string = core.getInput("milliseconds")
     core.debug(`Waiting ${ms} milliseconds ...`)
 
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput("time", new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
   }
