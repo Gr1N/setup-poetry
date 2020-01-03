637d4621594f05d66e8b12051bacf0016467027b
Nikita Grishko <gr1n@protonmail.com>
2020-01-03 21:10:28 +0300

    [chore] configure prettier

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,14 +1,14 @@
-import { wait } from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
+import { wait } from "../src/wait"
+import * as process from "process"
+import * as cp from "child_process"
+import * as path from "path"
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+test("throws invalid number", async () => {
+  const input = parseInt("foo", 10)
+  await expect(wait(input)).rejects.toThrow("milliseconds not a number")
 })
 
-test('wait 500 ms', async () => {
+test("wait 500 ms", async () => {
   const start = new Date()
   await wait(500)
   const end = new Date()
@@ -17,9 +17,9 @@ test('wait 500 ms', async () => {
 })
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+test("test runs", () => {
+  process.env["INPUT_MILLISECONDS"] = "500"
+  const ip = path.join(__dirname, "..", "lib", "main.js")
   const options: cp.ExecSyncOptions = {
     env: process.env
   }
