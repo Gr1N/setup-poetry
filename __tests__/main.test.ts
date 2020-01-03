a4bada360ef8fd744dd0f8f8438b95d044d189b8
Nikita Grishko <gr1n@protonmail.com>
2020-01-03 22:11:43 +0300

    [chore] cleanup template code

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,14 +1,3 @@
-import { wait } from "../src/wait"
-
-test("throws invalid number", async () => {
-  const input = parseInt("foo", 10)
-  await expect(wait(input)).rejects.toThrow("milliseconds not a number")
-})
-
-test("wait 500 ms", async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
+test("1+1", async () => {
+  expect(1 + 1).toBe(2)
 })
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 import * as core from "@actions/core"
-import { wait } from "./wait"
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput("milliseconds")
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput("time", new Date().toTimeString())
+    const test: string = core.getInput("test")
+    core.debug(`Test input value is ${test}`)
   } catch (error) {
     core.setFailed(error.message)
   }
